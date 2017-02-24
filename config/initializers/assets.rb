852f589133e244529008c1272988c45bc4147716
julesellaboranto <jfl-@gmx.de>
2017-02-24 18:52:26 +0100

    3.9 new stylesheet and modified files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,5 @@
+Rails.application.config.assets.precompile += %w( static_pages.css )
+
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
