cc1de29158cf42c7827c4ff261a0d6829fac4d4d
julesellaboranto <jfl-@gmx.de>
2017-03-08 18:09:57 +0100

    orders_controller landing_page routes

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,16 @@
+class OrdersController < ApplicationController #declares a class that inherits from ApplicationController, means it has access to all the methods inside application_controller.rb
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end	
+  
+  def destroy
+  end
+end
