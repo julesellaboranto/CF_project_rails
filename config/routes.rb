2f2a44b45dcf8f530171eedcac4db8ade5731a55
julesellaboranto <jfl-@gmx.de>
2017-03-09 22:05:02 +0100

    5.1 change root page, update links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
+    root 'static_pages#index'
+
   get 'static_pages/about'
 
   get 'static_pages/contact'
