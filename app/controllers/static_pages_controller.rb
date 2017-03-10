95253af47915da633dc02ef92f1852336b003bd6
julesellaboranto <jfl-@gmx.de>
2017-03-10 11:33:11 +0100

    fixed static_pages_controller.rb styling carousel

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,8 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
-
   def landing_page
-    @featured_product = Product.first
+    @products = Product.first
+    @products = Product.limit(3)
   end
-
-  def index
-  @products = Product.limit(3)
-  end
-
-
-
 
 
 end
