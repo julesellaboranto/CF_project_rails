62db3a9f614daf282701e4b8a9010f0f244a209a
julesellaboranto <jfl-@gmx.de>
2017-03-10 09:45:43 +0100

    change routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
-    root 'static_pages#index'
+    #root 'static_pages#index'
 
   get 'static_pages/about'
 
