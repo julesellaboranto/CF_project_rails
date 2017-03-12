2d9e79a67f4e2751f928d710eaf47aa4c2fffc85
julesellaboranto <jfl-@gmx.de>
2017-03-12 15:28:50 +0100

    5.4 changes in environment.rb and static_pages controller, added thank_you.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,16 @@ class StaticPagesController < ApplicationController
     @products = Product.limit(3)
   end
 
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  ActionMailer::Base.mail(:from => @email,
+      :to => 'jfl-@gmx.de',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+end
+
  #module CarouselHelper
   #def carousel_for(images)
     #Carousel.new(self, images).html
