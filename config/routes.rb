84f492682a2863f37fc355dc1294a4f1f4e881b8
julesellaboranto <jfl-@gmx.de>
2017-03-14 12:33:13 +0100

    change landing page in routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'static_pages/landing_page'
+
   post 'static_pages/thank_you'
 
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
