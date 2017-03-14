aaba679924d702f4c08bb1d2f613d00e2b0a77e2
julesellaboranto <jfl-@gmx.de>
2017-03-14 11:24:12 +0100

    fixed login errorin db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312194703) do
+ActiveRecord::Schema.define(version: 20170314085034) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
