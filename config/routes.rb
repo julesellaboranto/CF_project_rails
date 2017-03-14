2a7714465f29ad5ea0683338654cccd19ec5c28b
julesellaboranto <jfl-@gmx.de>
2017-03-14 09:24:47 +0100

    changed root in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,17 @@ Rails.application.routes.draw do
   resources :orders, only: [:index, :show, :create, :destroy]
   resources :users
   resources :products do
-    resources :comments
+  resources :comments
   end
   
-	root 'static_pages#landing_page'
-
-  get 'static_pages/about'
+	get 'static_pages/about'
 
   get 'static_pages/contact'
 
   get 'static_pages/index'
 
-  get 'static_pages/landing_page'
-
   post 'static_pages/thank_you'
+
+  root 'static_pages#landing_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
