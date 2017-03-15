28da2c590ff29f5e94879018f89794a1c6adebb3
julesellaboranto <jfl-@gmx.de>
2017-03-15 10:11:50 +0100

    change in user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+<<<<<<< Updated upstream
+=======
+
+  #rescue_from CanCan::AccessDenied do |exception|
+    #redirect_to main_app.root_url, :alert => exception.message
+  #end
+>>>>>>> Stashed changes
 end
