7339cfb5800dba2770e51eb8a5a890fa3255d0e5
julesellaboranto <jfl-@gmx.de>
2017-03-15 11:34:31 +0100

    change in js

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+/*$(document).on('turbolinks:load', function(){
   $('.notice').delay(2000).fadeIn(2000);
   $('.alert').delay(2000).fadeIn(2000);
-)}
+)}*/
