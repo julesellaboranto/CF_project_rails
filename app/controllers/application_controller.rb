7e71abe792c3ee75a345387dc898ecae08761f63
julesellaboranto <jfl-@gmx.de>
2017-03-16 15:50:07 +0100

    5.4: Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,3 +5,9 @@ class ApplicationController < ActionController::Base
   #end
 
 end
+
+class ApplicationController < ActionController::Base
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
+end
