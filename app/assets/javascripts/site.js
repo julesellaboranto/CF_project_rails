f75ddd371524342de0d9f177c8ca16c70483273d
julesellaboranto <jfl-@gmx.de>
2017-03-18 13:58:11 +0100

    5.8 star rating and comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,16 @@
+$('.alert').delay(2000).fadeOut(3000);
 $(document).on('turbolinks:load', function(){
-  $('.alert').delay(2000).fadeOut(3000);
-  $('.rating').raty( {path: '/assets'});
-})
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+      readOnly: true,
+      score: function() {
+        return $(this).attr('data-score');
+      }
+    });
+});
 
+
+  
 
 
 /*function hideAlert() {
--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,7 @@ end
 def lowest_rating_comment
     comments.rating_asc.first
 end
+def average_rating
+  comments.average(:rating).to_f
+end
 end
