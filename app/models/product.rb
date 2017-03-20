efc59ee4f0edd70602281fb7fdc71ce4883f9513
julesellaboranto <jfl-@gmx.de>
2017-03-20 14:17:02 +0100

    5.9: Validation & Pagination

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
+	validates :name, presence: true
+	validates :description, presence: true
+	validates :image_url, presence: true
+	validates :colour, presence: true
+	validates :price, presence: true
 
 def highest_rating_comment
 	comments.rating_desc.first
