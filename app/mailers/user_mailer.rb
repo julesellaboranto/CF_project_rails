ccdd8052ff44ed3aab5be15e0f8eb03520b6e695
julesellaboranto <jfl-@gmx.de>
2017-03-21 15:27:29 +0100

    5.11 Automated Email on User Sign Up

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,4 +7,10 @@ class UserMailer < ApplicationMailer
       :to => 'juliaf.landsiedel@gmail.com',
       :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  @appname = "Bikes Berlin"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+  end
 end
