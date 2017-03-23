b46cb7a90afab0d4d13bb507d5b1d76af88f8b30
julesellaboranto <jfl-@gmx.de>
2017-03-23 14:51:27 +0100

    5.11 change in about page, profile and editing product admin only, validates price & URL, product image atag

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,20 @@
 module ProductsHelper
 end
+
+
+class HttpUrlValidator < ActiveModel::EachValidator
+
+  def self.compliant?(value)
+    uri = URI.parse(value)
+    uri.is_a?(URI::HTTP) && !uri.host.nil?
+  rescue URI::InvalidURIError
+    false
+  end
+
+  def validate_each(record, attribute, value)
+    unless value.present? && self.class.compliant?(value)
+      record.errors.add(attribute, "is not a valid HTTP URL")
+    end
+  end
+
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ApplicationRecord
 	has_many :comments
 	validates :name, presence: true
 	validates :description, presence: true
-	validates :image_url, presence: true
+	validates :image_url, presence: true, http_url: true
 	validates :colour, presence: true
-	validates :price, presence: true
+	validates :price, presence: true, numericality: true
 
 def highest_rating_comment
 	comments.rating_desc.first
@@ -14,7 +14,9 @@ end
 def lowest_rating_comment
     comments.rating_asc.first
 end
+
 def average_rating
   comments.average(:rating).to_f
 end
+
 end
